bedf0c20ac9d402dbb520b27bfe08d196bc93492
saba fooladfar <fooladfarsaba@yahoo.com>
2022-11-09 20:28:18 +0330

    fourth commit

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,9 +1,24 @@
-const NavBar = ({unCompletedTodos}) => {
-    return ( 
-        <div>
-            Uncompleted Todos : <button>{unCompletedTodos}</button>
-        </div>
-     );
-}
- 
+import { useState } from "react";
+import Select from "react-select";
+
+const filterOptions = [
+  { value: "All", label: "All" },
+  { value: "Completed", label: "Completed" },
+  { value: "Uncompleted", label: "Uncompleted" },
+];
+const NavBar = ({ unCompletedTodos, onSelect, selectedOption}) => {
+  return (
+    <div>
+      Uncompleted Todos : <button>{unCompletedTodos}</button>
+      <div>
+        <Select
+          onChange={onSelect}
+          options={filterOptions}
+          value={selectedOption}
+        />
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
--- src/Component/TodoApp.js
+++ src/Component/TodoApp.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 import NavBar from "./NavBar";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("All");
+
+  useEffect(() => {
+    filteredTodos(selectedOption.value);
+  }, [todos, selectedOption]);
 
   const addTodo = (input) => {
     const newTodo = {
@@ -37,13 +44,39 @@ const TodoApp = () => {
     setTodos(updatedTodos);
   };
 
+  const filteredTodos = (status) => {
+    switch (status) {
+      case "Completed":
+        setFilter(todos.filter((t) => t.isCompleted));
+        break;
+      case "Uncompleted":
+        setFilter(todos.filter((t) => !t.isCompleted));
+        break;
+      default:
+        setFilter(todos);
+    }
+  };
+  const selectChangeHandler = (e) => {
+    filteredTodos(e.value);
+    setSelectedOption(e);
+  };
+
   return (
     <div>
-      <NavBar unCompletedTodos={todos.filter((t) => !t.isCompleted).length} />
+      <NavBar
+        unCompletedTodos={todos.filter((t) => !t.isCompleted).length}
+        onSelect={selectChangeHandler}
+        selectedOption={selectedOption}
+      />
+      {/* <Select
+        value={todos}
+        options={filterOptions}
+        onChange={filterHandler}
+      /> */}
       <div className="container">
         <TodoForm addTodoHandler={addTodo} />
         <TodoList
-          todos={todos}
+          todos={filter}
           onComplete={onComplete}
           onDelete={onDelete}
           onUpdate={updateTodo}
